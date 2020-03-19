91150e0dd1de0b243357d15be77e5154e8d8996c
Milja Niemi <milja.e.niemi@helsinki.fi>
2020-03-19 11:10:55 +0200

    Testit muokattu

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,38 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+        
+    @Test
+    public void saldoOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataRahaaOikein() {
+        kortti.lataaRahaa(20);
+        assertEquals("saldo: 30.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinKunRahaaTarpeeksi() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuKunRahaaEiTarpeeksi() {
+        kortti.otaRahaa(10);
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }
+    
+    @Test
+    public void metodiPalauttaaTrue() {
+        assertTrue(kortti.otaRahaa(2));
+    }
+    
+    @Test
+    public void metodiPalauttaaFalse() {
+        assertFalse(kortti.otaRahaa(30));
+    }
 }
