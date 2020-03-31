e8a4547efb1e0beb3f3bcfead47ca117f27cbffe
Milja <milja.e.niemi@helsinki.fi>
2020-03-31 21:32:34 +0300

    korjailua

--- StudyingCalendar/src/test/java/CourseTest.java
+++ StudyingCalendar/src/test/java/CourseTest.java
@@ -1,7 +1,9 @@
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
 import stydyingcalendar.Course;
+
 
 public class CourseTest {
     
